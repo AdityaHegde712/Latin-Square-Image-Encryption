75da2aca6f561c9964bec0f9e16bb96f1f9996e7
Harsh Morayya <harshmorayya@ymail.com>
2023-10-31 22:35:57 +0530

    Added Backend and Frontend

---
+++ prod/crypto-proj-front-end/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import MainForm from "./components/MainForm.vue";
+</script>
+
+<template>
+  <div>
+    <div
+      class="bg-slate-200 mx-auto rounded-b-2xl flex flex-col text-center justify-center h-[100px] w-[75%]"
+    >
+      <div class="pl-12">
+        <header>
+          <h1 class="text-7xl">Cryptography Project</h1>
+        </header>
+      </div>
+    </div>
+    <MainForm></MainForm>
+  </div>
+</template>
