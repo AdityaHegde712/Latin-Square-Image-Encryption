664f0478c013e840725d27562372ec7e660aaa1f
Aditya Hegde <adityahegde712@gmail.com>
2023-11-20 12:04:51 +0530

    bleep

--- prod/crypto-proj-back-end/mainBackend.py
+++ prod/crypto-proj-back-end/mainBackend.py
@@ -261,6 +261,92 @@ def LogisticEncryption(imageName, key):
     im.save(imageName.split('.')[0] + "_LogisticEnc.png", "PNG")
 
 
+# def LogisticDecryption(imageName, key):
+    # N = 256
+    # key_list = [ord(x) for x in key]
+
+    # G = [key_list[0:4] ,key_list[4:8], key_list[8:12]]
+    # g = []
+    # R = 1
+    # for i in range(1,4):
+    #     s = 0
+    #     for j in range(1,5):
+    #         s += G[i-1][j-1] * (10**(-j))
+    #     g.append(s)
+    #     R = (R*s) % 1
+    
+    # L_x = (R + key_list[12]/256) % 1
+    # S_x = round(((g[0]+g[1]+g[2])*(10**4) + L_x *(10**4)) % 256)
+    # V1 = sum(key_list)
+    # V2 = key_list[0]
+    # for i in range(1,13): # Here we declare how long we want our encryption key to be.
+    #     V2 = V2 ^ key_list[i]
+    # V = V2/V1
+
+    # L_y = (V+key_list[12]/256) % 1
+    # S_y = round((V+V2+L_y*10**4) % 256)
+    # C1_0 = S_x
+    # C2_0 = S_y
+    
+    # C = round((L_x*L_y*10**4) % 256)
+    # I_prev = C
+    # I_prev_r = C
+    # I_prev_g = C
+    # I_prev_b = C
+    # I = C
+    # I_r = C
+    # I_g = C
+    # I_b = C
+    # x_prev = 4*(S_x)*(1-S_x)
+    # y_prev = 4*(L_x)*(1-S_y)
+    # x = x_prev
+    # y = y_prev
+    # imageName += ".png"
+    # imageMatrix,dimensionX, dimensionY, color = getImageMatrix(imageName)
+
+    # logisticDecryptedImage = []
+    # for i in range(dimensionX):
+    #     row = []
+    #     for j in range(dimensionY):
+    #         while x <0.8 and x > 0.2 :
+    #             x = 4*x*(1-x)
+    #         while y <0.8 and y > 0.2 :
+    #             y = 4*y*(1-y)
+    #         x_round = round((x*(10**4))%256)
+    #         y_round = round((y*(10**4))%256)
+    #         C1 = x_round ^ ((key_list[0]+x_round) % N) ^ ((C1_0 + key_list[1])%N)
+    #         C2 = x_round ^ ((key_list[2]+y_round) % N) ^ ((C2_0 + key_list[3])%N) 
+    #         if color:
+    #             I_r = ((((key_list[4]+C1) % N) ^ ((key_list[5]+C2) % N) ^ ((I_prev_r + key_list[7]) % N) ^ imageMatrix[i][j][0]) + N-key_list[6])%N
+    #             I_g = ((((key_list[4]+C1) % N) ^ ((key_list[5]+C2) % N) ^ ((I_prev_g + key_list[7]) % N) ^ imageMatrix[i][j][1]) + N-key_list[6])%N
+    #             I_b = ((((key_list[4]+C1) % N) ^ ((key_list[5]+C2) % N) ^ ((I_prev_b + key_list[7]) % N) ^ imageMatrix[i][j][2]) + N-key_list[6])%N
+    #             I_prev_r = imageMatrix[i][j][0]
+    #             I_prev_g = imageMatrix[i][j][1]
+    #             I_prev_b = imageMatrix[i][j][2]
+    #             row.append((I_r,I_g,I_b))
+    #             x = (x +  imageMatrix[i][j][0]/256 + key_list[8]/256 + key_list[9]/256) % 1
+    #             y = (x +  imageMatrix[i][j][0]/256 + key_list[8]/256 + key_list[9]/256) % 1  
+    #         else:
+    #             I = ((((key_list[4]+C1) % N) ^ ((key_list[5]+C2) % N) ^ ((I_prev+key_list[7]) % N) ^ imageMatrix[i][j]) + N-key_list[6])%N
+    #             I_prev = imageMatrix[i][j]
+    #             row.append(I)
+    #             x = (x +  imageMatrix[i][j]/256 + key_list[8]/256 + key_list[9]/256) % 1
+    #             y = (x +  imageMatrix[i][j]/256 + key_list[8]/256 + key_list[9]/256) % 1
+    #         for ki in range(12):
+    #             key_list[ki] = (key_list[ki] + key_list[12]) % 256
+    #             key_list[12] = key_list[12] ^ key_list[ki]
+    #     logisticDecryptedImage.append(row)
+    # if color:
+    #     im = Image.new("RGB", (dimensionX, dimensionY))
+    # else: 
+    #     im = Image.new("L", (dimensionX, dimensionY)) # L is for Black and white pixels
+    # pix = im.load()
+    # for x in range(dimensionX):
+    #     for y in range(dimensionY):
+    #         pix[x, y] = logisticDecryptedImage[x][y]
+    # im.save(imageName.split('_')[0] + "_LogisticDec.png", "PNG")
+
+
 def LogisticDecryption(imageName, key):
     N = 256
     key_list = [ord(x) for x in key]
@@ -268,27 +354,28 @@ def LogisticDecryption(imageName, key):
     G = [key_list[0:4] ,key_list[4:8], key_list[8:12]]
     g = []
     R = 1
-    for i in range(1,4):
+    for i in range(1, 4):
         s = 0
-        for j in range(1,5):
+        for j in range(1, 5):
             s += G[i-1][j-1] * (10**(-j))
         g.append(s)
-        R = (R*s) % 1
+        R = (R * s) % 1
     
     L_x = (R + key_list[12]/256) % 1
-    S_x = round(((g[0]+g[1]+g[2])*(10**4) + L_x *(10**4)) % 256)
+    S_x = round(((g[0] + g[1] + g[2]) * (10*4) + L_x * (10*4)) % 256)
     V1 = sum(key_list)
     V2 = key_list[0]
-    for i in range(1,13): # Here we declare how long we want our encryption key to be.
+    for i in range(1, 13):
         V2 = V2 ^ key_list[i]
-    V = V2/V1
 
-    L_y = (V+key_list[12]/256) % 1
-    S_y = round((V+V2+L_y*10**4) % 256)
+    V = V2 / V1
+
+    L_y = (V + key_list[12]/256) % 1
+    S_y = round((V + V2 + L_y * 10**4) % 256)
     C1_0 = S_x
     C2_0 = S_y
     
-    C = round((L_x*L_y*10**4) % 256)
+    C = round((L_x * L_y * 10**4) % 256)
     I_prev = C
     I_prev_r = C
     I_prev_g = C
@@ -297,49 +384,48 @@ def LogisticDecryption(imageName, key):
     I_r = C
     I_g = C
     I_b = C
-    x_prev = 4*(S_x)*(1-S_x)
-    y_prev = 4*(L_x)*(1-S_y)
+    x_prev = 4 * (S_x) * (1 - S_x)
+    y_prev = 4 * (L_x) * (1 - S_y)
     x = x_prev
     y = y_prev
-    imageName += ".png"
-    imageMatrix,dimensionX, dimensionY, color = getImageMatrix(imageName)
+    imageMatrix, dimensionX, dimensionY, color = getImageMatrix(imageName)
 
     logisticDecryptedImage = []
-    for i in range(dimensionX):
+    for i in range(dimensionX - 1, -1, -1):  # Updated iteration range
         row = []
         for j in range(dimensionY):
-            while x <0.8 and x > 0.2 :
-                x = 4*x*(1-x)
-            while y <0.8 and y > 0.2 :
-                y = 4*y*(1-y)
-            x_round = round((x*(10**4))%256)
-            y_round = round((y*(10**4))%256)
-            C1 = x_round ^ ((key_list[0]+x_round) % N) ^ ((C1_0 + key_list[1])%N)
-            C2 = x_round ^ ((key_list[2]+y_round) % N) ^ ((C2_0 + key_list[3])%N) 
+            while x < 0.8 and x > 0.2:
+                x = 4 * x * (1 - x)
+            while y < 0.8 and y > 0.2:
+                y = 4 * y * (1 - y)
+            x_round = round((x * (10**4)) % 256)
+            y_round = round((y * (10**4)) % 256)
+            C1 = x_round ^ ((key_list[0] + x_round) % N) ^ ((C1_0 + key_list[1]) % N)
+            C2 = x_round ^ ((key_list[2] + y_round) % N) ^ ((C2_0 + key_list[3]) % N) 
             if color:
-                I_r = ((((key_list[4]+C1) % N) ^ ((key_list[5]+C2) % N) ^ ((I_prev_r + key_list[7]) % N) ^ imageMatrix[i][j][0]) + N-key_list[6])%N
-                I_g = ((((key_list[4]+C1) % N) ^ ((key_list[5]+C2) % N) ^ ((I_prev_g + key_list[7]) % N) ^ imageMatrix[i][j][1]) + N-key_list[6])%N
-                I_b = ((((key_list[4]+C1) % N) ^ ((key_list[5]+C2) % N) ^ ((I_prev_b + key_list[7]) % N) ^ imageMatrix[i][j][2]) + N-key_list[6])%N
+                I_r = ((((key_list[4] + C1) % N) ^ ((key_list[5] + C2) % N) ^ ((I_prev_r + key_list[7]) % N) ^ imageMatrix[i][j][0]) + N - key_list[6]) % N
+                I_g = ((((key_list[4] + C1) % N) ^ ((key_list[5] + C2) % N) ^ ((I_prev_g + key_list[7]) % N) ^ imageMatrix[i][j][1]) + N - key_list[6]) % N
+                I_b = ((((key_list[4] + C1) % N) ^ ((key_list[5] + C2) % N) ^ ((I_prev_b + key_list[7]) % N) ^ imageMatrix[i][j][2]) + N - key_list[6]) % N
                 I_prev_r = imageMatrix[i][j][0]
                 I_prev_g = imageMatrix[i][j][1]
                 I_prev_b = imageMatrix[i][j][2]
-                row.append((I_r,I_g,I_b))
-                x = (x +  imageMatrix[i][j][0]/256 + key_list[8]/256 + key_list[9]/256) % 1
-                y = (x +  imageMatrix[i][j][0]/256 + key_list[8]/256 + key_list[9]/256) % 1  
+                row.append((I_r, I_g, I_b))
+                x = (x +  imageMatrix[i][j][0] / 256 + key_list[8] / 256 + key_list[9] / 256) % 1
+                y = (x +  imageMatrix[i][j][0] / 256 + key_list[8] / 256 + key_list[9] / 256) % 1  
             else:
-                I = ((((key_list[4]+C1) % N) ^ ((key_list[5]+C2) % N) ^ ((I_prev+key_list[7]) % N) ^ imageMatrix[i][j]) + N-key_list[6])%N
+                I = ((((key_list[4] + C1) % N) ^ ((key_list[5] + C2) % N) ^ ((I_prev + key_list[7]) % N) ^ imageMatrix[i][j]) + N - key_list[6]) % N
                 I_prev = imageMatrix[i][j]
                 row.append(I)
-                x = (x +  imageMatrix[i][j]/256 + key_list[8]/256 + key_list[9]/256) % 1
-                y = (x +  imageMatrix[i][j]/256 + key_list[8]/256 + key_list[9]/256) % 1
+                x = (x +  imageMatrix[i][j] / 256 + key_list[8] / 256 + key_list[9] / 256) % 1
+                y = (x +  imageMatrix[i][j] / 256 + key_list[8] / 256 + key_list[9] / 256) % 1
             for ki in range(12):
                 key_list[ki] = (key_list[ki] + key_list[12]) % 256
-                key_list[12] = key_list[12] ^ key_list[ki]
+            key_list[12] = key_list[12] ^ key_list[ki]
         logisticDecryptedImage.append(row)
     if color:
         im = Image.new("RGB", (dimensionX, dimensionY))
     else: 
-        im = Image.new("L", (dimensionX, dimensionY)) # L is for Black and white pixels
+        im = Image.new("L", (dimensionX, dimensionY))  # L is for Black and white pixels
     pix = im.load()
     for x in range(dimensionX):
         for y in range(dimensionY):
@@ -366,6 +452,7 @@ def LatinSqDec2(C,K):
 LE = None
 def encryptTotal(gray):
     global LE
+
     # Encryption
     LE = LatinSqEnc2(gray)
     cv2.imwrite('buffer/aa_gray_enc.png', LE[0])
@@ -390,10 +477,9 @@ def decryptTotal(filename = "buffer/aa_gray_enc_LogisticEnc"):
     return LD
 
 
-if __name__ == "__main__":
-    img = cv2.imread('Image-Encryption-using-Latin-Square-Matrix-and-Logistic-Map\\test_images\\aa_gray.jpg')
+if __name__ == "__main__":\
+    # read aa_gray.png and store as array of values
+    img = cv2.imread('Image-Encryption-using-Latin-Square-Matrix-and-Logistic-Map\\test_images\\aa_gray.png')
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray = cv2.resize(gray, (256,256))
-
     encryptTotal(gray)
     decryptTotal()
